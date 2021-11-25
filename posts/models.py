a42030ab4a54f9fdae028aa820a2153a99d04be9
jaimeandresalas <jsalasmoreira@gmail.com>
2021-11-25 15:52:28 -0500

    Agregando modelo de datos

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Django
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True)
+
+    birthday = models.DateField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
 # Create your views here.
 posts = [
-  {
-    'name': 'Mont Blac',
-    'user': 'Yesica Cortes',
-    'timestamp': datetime.now().strftime('%Y-%m-%d'),
-    'picture': 'https://evalart.com/wp-content/uploads/2019/03/PruebasProgramacion.png'
-  }
+    {
+        'title': 'Mont Blac',
+        'user': {'name': 'Yesica Cortes',
+                 'picture': 'https://evalart.com/wp-content/uploads/2019/03/PruebasProgramacion.png'
+                 },
+        'timestamp': datetime.now().strftime('%Y-%m-%d'),
+
+    }
 ]
 
 
 def list_posts(request):
-  """List all posts"""
-  content = []
-  for post in posts:
-    content.append("""
-                   <p><strong>{name}</strong></p>
-                   <p><strong>{user}</strong></p>
-                   <p><strong>{timestamp}</strong></p>
-                   <figure><img src="{picture}/"></figure>
-                   """.format(**post))
-  return HttpResponse('<br>'.join(content))
+    """List all posts"""
+    return render(request, 'feed.html', {'posts': posts})
